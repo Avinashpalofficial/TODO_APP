2845a6c9e5f810bd78489271069d23b35bbe9ec0
Avinashpalofficial <apal73451@gmail.com>
2025-07-17 17:42:18 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,15 @@
 const express = require('express')
+const { creaeTodo } = require('./type')
 const app = express()
 
 const port = 3000
-
+app.use(express.json())
 app.post('/todo', (req,res)=>{
-
+     const createpayload = req.body
+     const parsepayload = creaeTodo.safeParse(createpayload)
+     if(!parsepayload.succes){
+        
+     }
 
 })
   
