f75e13702ba3a159eba32361d952c7f32ec7d112
Avinashpalofficial <apal73451@gmail.com>
2025-07-18 14:11:18 +0530

    add frontend

---
+++ frontend/src/Component/Componenttodo.jsx
@@ -0,0 +1,7 @@
+  export function Createtodo(){
+    return <div>
+        <input type="text" placeholder="title" /> <br />
+        <input type="text" placeholder="description" /> <br />
+        <button>ADD a todo</button>
+    </div>
+ }
